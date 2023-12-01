a226e06cd378b83a6342734195032476efa95b6d
feryndka <feryandika151@gmail.com>
2023-12-01 09:04:56 +0700

    fix contact(2)

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -10,7 +10,7 @@ export default function Contact() {
         <div className="contact">
             <Navbar />
             <Particle />
-            <div className='page4 h-auto flex justify-center items-center'>
+            <div className='page4 h-screen flex items-center'>
                 <div className='card max-w-md w-4/5 mx-auto p-6 rounded-lg shadow-md'>
                     <h2 className='text-3xl text-center font-bold mb-6 underline underline-offset-8'>Contact Me</h2>
                     <form action="" method=''>
