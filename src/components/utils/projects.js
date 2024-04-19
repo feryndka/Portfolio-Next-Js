79ba1b39b3dd6e6e2003217fc815c26c5df19bd9
feryndka <feryandika151@gmail.com>
2024-04-20 00:01:44 +0700

    build:project dashboard admin

--- src/components/utils/projects.js
+++ src/components/utils/projects.js
@@ -40,28 +40,25 @@ export const projects = [
     github: "https://github.com/feryndka/web-anime",
     webapp: "https://anime-idn.vercel.app",
   },
-  // {
-  //   id: 4,
-  //   title: "Coffee Shop",
-  //   date: "Feb 2024 - Mar 2024",
-  //   description:
-  //     "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
-  //   image:
-  //     "/project/coffee-shop.JPG",
-  //   tags: [
-  //     "Docker",
-  //     "AWS",
-  //     "DuckDNS",
-  //     "Eslint",
-  //     "Husky",
-  //     "CI/CD",
-  //     "React Js",
-  //     "MongoDb",
-  //     "Node Js",
-  //     "Express Js",
-  //     "Redux",
-  //   ],
-  //   github: "https://github.com/rishavchanda/Trackify",
-  //   webapp: "https://trackify-management.netlify.app/",
-  // },
+  {
+    id: 4,
+    title: "Dashboard Admin",
+    date: "Jan 2024 - Feb 2024",
+    description:
+      "Build Dashboard admin with next Js 14, and database mysql.",
+    image:
+      "/project/admin-dashboard.JPG",
+    tags: [
+      "React Js",
+      "Next Js",
+      "Axios",
+      "Mysql",
+      "Yup",
+      "Typescript",
+      "Tailwind Css",
+      "Material UI",
+    ],
+    github: "https://github.com/feryndka/Smart_Library_FE",
+    webapp: "https://smart-library-school.vercel.app/dashboard",
+  },
 ];
